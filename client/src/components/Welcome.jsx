99e37ec0e005e92ef6b90b80bc18e313c150e21d
Harish Neel <contact@harishcode.com>
2023-05-09 19:32:50 +0530

    Done styling the navbar, including making it responsive for smaller devices. Followed a mobile-first approach

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,16 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
+
 const Welcome = () => {
     return (
-        <div>Welcome</div>
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                {/* <div className="flex flex-1 justify-start flex-col md" */}
+            </div>
+        </div>
     )
 }
 
