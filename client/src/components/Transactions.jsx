34ec7c00ca4a39ad4b1096bcc528777765c9a6bf
Harish Neel <contact@harishcode.com>
2023-05-13 17:39:49 +0530

    giphy api linked done

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -4,6 +4,42 @@ import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
 
 
+const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+    return (
+        <div className='bg-[#181918] m-4 flex flex-1
+            2xl: min-w-[450px]
+            2xl:max-w-[500px]
+            sm:min-w-[270px]
+            sm:max-w-[300px]
+            flex-col p=3 rounded-md hover:shadow-2xl
+        '>
+            <div className='flex flex-col items-center w-full mt-3'>
+                <div className='w-full mb-6 p-2'>
+                    <a href = {`https://goerli.etherscan.io/address/${addressFrom}`} target = "_blank" rel = "noopener noreferrer" >
+                        <p className='text-white text-base'> From: {shortenAddress(addressFrom)}</p>
+                    </a>
+                    <a href = {`https://goerli.etherscan.io/address/${addressTo}`} target = "_blank" rel = "noopener noreferrer" >
+                        <p className='text-white text-base'> To: {shortenAddress(addressTo)}</p>
+                    </a>
+                    <p className='text-white text-base'> Amount: {amount} ETH</p> 
+                    { 
+                        message && (
+                            <>
+                                <br />
+                                <p className='text-white text-base'>Message: {message}</p>
+                            </>
+                        )
+                    }
+
+                    <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadhow-2xl'>
+                        <p className='text-[#37c7da] font-bold'>{timestamp}</p>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    )
+}
+
 const Transactions = () => {
 
     const {currentAccount} = useContext(TransactionContext);
@@ -15,6 +51,12 @@ const Transactions = () => {
                     <h3 className='text-white text-3xl text-center my-2'>Latest transactions</h3>
                 ): <h3 className='text-white text-3xl text-center my-2'>Connect your account to see the latest transactions</h3>
             }
+
+            <div className='flex flex-wrap justify-center items-center mt-10'>
+                {[dummyData.reverse().map((transaction, i) => {
+                    return (<TransactionCard key = {i} {...transaction}/> )
+                })]}
+            </div>
                 {/* Latest Transactions if we have account connected */}
                 {/* Connect your account to seee the Latest transactions */}
             </div>
