e9f3875d731c3e841ebdbc5456ac1e0de1ac998e
Harish Neel <contact@harishcode.com>
2023-05-13 16:55:39 +0530

    current accounts address has been shown on the card above in a format similar to how metamask shows it

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,15 +1,23 @@
 import React, {useContext} from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 import dummyData from '../utils/dummyData';
+import { shortenAddress } from '../utils/shortenAddress';
 
 
 const Transactions = () => {
 
-
-
+    const {currentAccount} = useContext(TransactionContext);
+ 
     return (
-        <div className='flex'>
-
+        <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions'>
+            <div className='flex flex-col md:p-12 py-12 px-4'>
+                {currentAccount ? (
+                    <h3 className='text-white text-3xl text-center my-2'>Latest transactions</h3>
+                ): <h3 className='text-white text-3xl text-center my-2'>Connect your account to see the latest transactions</h3>
+            }
+                {/* Latest Transactions if we have account connected */}
+                {/* Connect your account to seee the Latest transactions */}
+            </div>
         </div>
     )
 }
