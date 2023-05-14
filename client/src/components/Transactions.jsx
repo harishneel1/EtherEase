f564fe99c38199c1ac0934390b0389adda93ec8a
Harish Neel <contact@harishcode.com>
2023-05-14 14:34:51 +0530

    useFetch hook written to fetch gifs and tested with dummy data

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -2,9 +2,10 @@ import React, {useContext} from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
-
+import useFetch from '../hooks/useFetch';
 
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+    const gifUrl = useFetch({keyword})
     return (
         <div className='bg-[#181918] m-4 flex flex-1
             2xl: min-w-[450px]
@@ -31,10 +32,16 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
                         )
                     }
 
-                    <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadhow-2xl'>
-                        <p className='text-[#37c7da] font-bold'>{timestamp}</p>
-                    </div>
                 </div>  
+                <img 
+                    src = {gifUrl || url}
+                    alt = "gif"
+                    className='w-full h-64 2x:h-96 rounded-md shadow-lg object-cover'
+                />
+
+                <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadhow-2xl'>
+                    <p className='text-[#37c7da] font-bold'>{timestamp}</p>
+                </div>
             </div>
         </div>
     )
