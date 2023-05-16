1eb4f61b5ccaf323266a0fa1ba5036f94d465abc
Harish Neel <contact@harishcode.com>
2023-05-17 00:07:20 +0530

    refactor

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -111,7 +111,7 @@ const Welcome = () => {
                                 <button 
                                     type = "button"
                                     onClick={handleSubmit}
-                                    className = "text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                                    className = "text-white  w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
                                 > Send now
 
                                 </button>
